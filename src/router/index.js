4f2e13c260745ea539d64a5f66b70af9dfd130d3
Nguyen Thanh Danh <nguyenthanhdanh221102@gmail.com>
2023-05-04 16:15:01 +0700

    bookreview-frontend

--- src/router/index.js
+++ src/router/index.js
@@ -46,7 +46,7 @@ const routes = [
   {
     path: "/bookdetail/:id",
     name: "bookdetail",
-    component: () => import("@/views/BookDetail.vue"),
+    component: () => import("@/components/BookDetail.vue"),
     props: true,
   },
 ];
